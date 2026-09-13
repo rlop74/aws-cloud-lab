09d4c62f9d5f55a6ab0532e8945016dfe09aa53b
rlop74 <119091421+rlop74@users.noreply.github.com>
2026-09-13 14:55:02 -0400

    feat: add PostgreSQL database health check

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import os
+from dotenv import load_dotenv
+import psycopg
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 app = FastAPI()
 
@@ -9,3 +20,26 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
+@app.get("/db-health")
+def db_health():
+    try:
+        with psycopg.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            connect_timeout=3
+        ) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT version()")
+                db_version=cur.fetchone()
+                print(db_version)
+                return {"db_status":"healthy"}
+    except psycopg.Error as e:
+        print(e)
+        raise HTTPException(
+            status_code=503,
+            detail="Database unavailable"
+      )
